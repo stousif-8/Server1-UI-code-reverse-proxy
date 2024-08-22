282662266d3fcd39728e6ae05e4baf32b49be8ae
root <root@ip-172-31-25-97.ap-southeast-1.compute.internal>
2024-08-22 11:36:10 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Fetch data from the backend API
+    fetch('/api/message')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{message ? message : 'Loading...'}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
